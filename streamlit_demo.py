e50de829707e489314331be5a1644ff3fc446c1a
Sagnik Mukherjee <57945314+sagnik0712mukherjee@users.noreply.github.com>
2025-05-14 11:39:49 +0530

    Update streamlit_demo.py

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,19 +1,14 @@
-import os
-from langchain_openai import ChatOpenAI
+from langchain_community.chat_models import ChatOllama
 import streamlit as st
+from langchain.globals import set_debug
+
+set_debug(True)
 
 st.title("Ask Me Anything 😉")
 
-with st.sidebar:
-    st.title("Enter your OpanAI API KEY")
-    OPENAI_API_KEY = st.text_input("OpenAI API Key", type="password")
-
-if not OPENAI_API_KEY:
-    st.info("You cannot proceed without putting your API key")
-    st.stop()
-
-llm=ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
 question = st.text_input("Enter the question here... ")
+
+llm=ChatOllama(model="llama3.2:latest")
 
 if question and llm:
     response = llm.invoke(question)
